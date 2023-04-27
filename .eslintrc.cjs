cbe532e5cbb86155adad3e220744fe68ee45f2f7
beyondme121 <beyondme121@163.com>
2023-04-27 22:56:22 +0800

    config .eslintrc.cjs & tsconfig.json & vite.config.ts

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parser: 'vue-eslint-parser', // new
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser', // new
+    tsconfigRootDir: __dirname, // new
+    project: ['tsconfig.json'], // new
+    extraFileExtensions: ['.vue'] // new
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint' // new
+  ],
+  rules: {
+    '@typescript-eslint/triple-slash-reference': 'off' // new: 解决vite-env.d.ts报错
+  }
+}
